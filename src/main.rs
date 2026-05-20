83d61722952b790f3d6019299d1bc286160b8184
Jhons Albornoz Araya <jhons.albornoz.araya@gmail.com>
2026-05-20 16:16:57 -0400

    add: se agrega el recibir argumentos por comandos y lectura de archivos package de el proyecto en cuestion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-use std::fs;
+use clap::Parser;
+use std::{fs, path::Path};
 
-fn main() {
-    let package_json_content = fs::read_to_string("")
-        .expect("Algo salio mal leyendo el archivo");
-
-    let package_lock_json_content = fs::read_to_string("n")
-        .expect("Algo salio mal leyendo el archivo");
-
-    println!("Contenido del package.json: {}", package_json_content);
-    println!("Contenido del package-lock.json: {}", package_lock_json_content);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Project path
+    #[arg(short, long)]
+    project_path: String,
 }
 
+fn main() {
+    let args = Args::parse();
+
+    let project_path = Path::new(&args.project_path);
+
+    let package_json_path = project_path.join("package.json");
+
+    if !package_json_path.exists() {
+        eprintln!("Error: package.json not found in the specified project path.");
+        std::process::exit(1);
+    }
+
+    let package_lock_json_path = project_path.join("package-lock.json");
+
+    if !package_lock_json_path.exists() {
+        eprintln!("Error: package-lock.json not found in the specified project path.");
+        std::process::exit(1);
+    }
+
+    let package_json_content =
+        fs::read_to_string(&package_json_path).expect("Error leyendo package.json");
+
+    let package_lock_json_content =
+        fs::read_to_string(&package_lock_json_path).expect("Error leyendo package-lock.json");
+
+    println!("{}", package_json_content);
+    println!("{}", package_lock_json_content);
+}
