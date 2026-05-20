16ee53b3620d6122bb88de7f0a3b5e5d278adb2f
Jhons Albornoz Araya <jhons.albornoz.araya@gmail.com>
2026-05-20 16:56:17 -0400

    add: se integra serde y serde_json para parsear JSON en main.rs y poder leer archivos

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::{fs, path::Path};
+use serde::Deserialize;
+use std::{collections::HashMap, fs, path::Path};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,20 +10,38 @@ struct Args {
     project_path: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct PackageJson {
+    name: String,
+    dependencies: Option<HashMap<String, String>>,
+    scripts: Option<HashMap<String, String>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageLockJson {
+    name: Option<String>,
+    version: Option<String>,
+    packages: Option<HashMap<String, LockPackage>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct LockPackage {
+    version: Option<String>,
+    resolved: Option<String>,
+    integrity: Option<String>,
+}
+
 fn main() {
     let args = Args::parse();
-
     let project_path = Path::new(&args.project_path);
 
     let package_json_path = project_path.join("package.json");
-
     if !package_json_path.exists() {
         eprintln!("Error: package.json not found in the specified project path.");
         std::process::exit(1);
     }
 
     let package_lock_json_path = project_path.join("package-lock.json");
-
     if !package_lock_json_path.exists() {
         eprintln!("Error: package-lock.json not found in the specified project path.");
         std::process::exit(1);
@@ -31,9 +50,15 @@ fn main() {
     let package_json_content =
         fs::read_to_string(&package_json_path).expect("Error leyendo package.json");
 
+    let package_json: PackageJson =
+        serde_json::from_str(&package_json_content).expect("Error parsing package.json");
+
     let package_lock_json_content =
         fs::read_to_string(&package_lock_json_path).expect("Error leyendo package-lock.json");
 
-    println!("{}", package_json_content);
-    println!("{}", package_lock_json_content);
+    let package_lock_json: PackageLockJson =
+        serde_json::from_str(&package_lock_json_content).expect("Error parsing package-lock.json");
+
+    println!("{:?}", package_json);
+    println!("{:?}", package_lock_json);
 }
